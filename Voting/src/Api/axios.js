0e2f446e34e9bf44612e9aaa6c6c11730a15eb45
Mohd Wali <mhwali625@gmail.com>
2025-07-31 18:54:01 +0530

    First

--- Voting/src/Api/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-// import {config} from 'dotenv';
-const API=axios.create({
-    baseURL:'http://localhost:3000',
-    headers:{
-        'Content-Type':'application/json',
-    },
-});
-export default API;
