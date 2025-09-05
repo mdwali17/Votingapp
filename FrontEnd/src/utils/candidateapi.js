e07c4eb90de112607643bfbd145734cfafed5cfa
Mohd Wali <mhwali625@gmail.com>
2025-09-05 19:52:51 +0530

    url fix

--- FrontEnd/src/utils/candidateapi.js
+++ FrontEnd/src/utils/candidateapi.js
@@ -1,4 +1,4 @@
-const BASE = 'https://voting-6qcx.onrender.com/'
+const BASE = 'https://voting-6qcx.onrender.com'
 const getAuthHeaders = () => {
   const token = localStorage.getItem('token');
   return {
