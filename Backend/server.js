dbaf04f44eda7221b7943e084a2211de5b93f04f
Mohd Wali <mhwali625@gmail.com>
2025-09-06 21:46:27 +0530

    ..

--- Backend/server.js
+++ Backend/server.js
@@ -57,7 +57,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: [ "https://votingapp17.netlify.app/" ], // add production domain later
+  origin: [ "https://votingapp17.netlify.app" ], // add production domain later
   credentials: true
 }));
 
